94a2cf174a68d797ccc679b27b851fc24dbcf467
gaurishanker <gaurishankerpctebtech2020@gmail.com>
2024-04-15 16:37:27 +0530

    some changes

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const dbConnection=()=>{
-    mongoose.connect(process.env.MONGO_URL,{
+    mongoose.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.5",{
         dbName:"Job",
     }).then(()=>{
         console.log("MongoDB Connected...")
